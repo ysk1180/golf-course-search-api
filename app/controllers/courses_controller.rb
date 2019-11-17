c1eae385cb787151f5c8e9d30f8494c565945cdc
Yusuke Miyazaki <ysk1180@gmail.com>
2019-11-17 19:15:07 +0900

    距離でフィルタリングできるようにした

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -3,7 +3,7 @@ class CoursesController < ApplicationController
     date = params['date']
     budget = params['budget']
     departure = params['departure']
-    duration = params['duration']
+    duration = params['duration'].to_i
 
     RakutenWebService.configure do |c|
       c.application_id = ENV['RAKUTEN_APPID']
@@ -13,17 +13,22 @@ class CoursesController < ApplicationController
     courses = RakutenWebService::Gora::Plan.search(maxPrice: budget, playDate: date, areaCode: '11,12,13,14', sort: 'evaluation')
     course_names = courses.map { |course| course['golfCourseName'] }
 
-    p departure
-    p date
-    p duration
-    # gmaps = GoogleMapsService::Client.new(key: ENV['GOOGLE_MAP_API_KEY'])
-    #
-    # routes = gmaps.directions(
-    #   '二子玉川駅',
-    #   'そうぶファミリーゴルフ',
-    # )
-    # p routes.first[:legs][0][:duration][:text]
+    gmaps = GoogleMapsService::Client.new(key: ENV['GOOGLE_MAP_API_KEY'])
 
-    render json: { course_names: course_names }
+    matched_courses_names = []
+    course_names.each do |name|
+      routes = gmaps.directions(
+        departure,
+        name,
+      )
+      duration_seconds = routes.first[:legs][0][:duration][:value]
+      duration_minutes = duration_seconds / 60
+      if duration_minutes < duration
+        matched_courses_names << name
+        break if matched_courses_names.size >= 5
+      end
+    end
+
+    render json: { course_names: matched_courses_names }
   end
 end
